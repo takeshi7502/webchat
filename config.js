59b1480253369313a567b0c9f7a0ab6aa0074402
Takeshi <takeshi.gi75@gmail.com>
2025-02-09 10:40:59 +0700

    Update cf

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_...", // Thay giá trị từ Firebase
+    authDomain: "takehi-webchat.firebaseapp.com",
+    databaseURL: "https://takehi-webchat-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "takehi-webchat",
+    storageBucket: "takehi-webchat.appspot.com",
+    messagingSenderId: "683823627022",
+    appId: "1:683823627022:web:0b542b89002bb723ae755f",
+    measurementId: "G-CBQ51RCJQD"
+};
